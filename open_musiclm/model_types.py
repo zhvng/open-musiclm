f8eeff19f6efaf6212c0d5f5edbb8e9abb6c5b03
zhvng <allenjameszhang@gmail.com>
2023-02-21 00:58:46 +0000

    remove fairseq wav2vec from wav2vec type

--- open_musiclm/model_types.py
+++ open_musiclm/model_types.py
@@ -1,8 +1,8 @@
-from audiolm_pytorch import FairseqVQWav2Vec, HubertWithKmeans, SoundStream
+from audiolm_pytorch import HubertWithKmeans, SoundStream
 from open_musiclm.hf_hubert_kmeans import HfHubertWithKmeans
 from beartype.typing import Union
 
 from .encodec_wrapper import EncodecWrapper
 
-Wav2Vec = Union[FairseqVQWav2Vec, HubertWithKmeans, HfHubertWithKmeans]
+Wav2Vec = Union[HubertWithKmeans, HfHubertWithKmeans]
 NeuralCodec = Union[SoundStream, EncodecWrapper]
