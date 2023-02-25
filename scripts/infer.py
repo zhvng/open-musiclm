74e67484e891176efe737be8824fa26b58ad8c1f
zhvng <allenjameszhang@gmail.com>
2023-02-25 21:19:54 +0000

    make script easier to use

--- scripts/infer.py
+++ scripts/infer.py
@@ -1,95 +1,151 @@
+'''
+example usage:
+
+python3 scripts/infer.py \
+  --semantic_path ./results/semantic/semantic.transformer.10000.pt \
+  --coarse_path ./results/coarse/coarse.transformer.10000.pt \
+  --fine_path ./results/fine/fine.transformer.10000.pt \
+  --return_coarse_wave
+'''
+
 import os
 import sys
 from pathlib import Path
 
 import torch
 import torchaudio
-from audiolm_pytorch import HubertWithKmeans
 from einops import rearrange
-
+import argparse
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from open_musiclm.clap_quantized import create_clap_quantized
 from open_musiclm.open_musiclm import create_coarse_transformer, create_fine_transformer, create_semantic_transformer, MusicLM
 from open_musiclm.encodec_wrapper import create_encodec_24khz
-from open_musiclm.trainer import SingleStageTrainer
-from scripts.train_utils import disable_print, get_wav2vec
+from open_musiclm.hf_hubert_kmeans import get_hubert_kmeans
+from scripts.train_utils import disable_print
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+prompts = [
+    [
+        'The main soundtrack of an arcade game. It is fast-paced and upbeat, with a catchy electric guitar riff. The music is repetitive and easy to remember, but with unexpected sounds, like cymbal crashes or drum rolls.',
+        'A fusion of reggaeton and electronic dance music, with a spacey, otherworldly sound. Induces the experience of being lost in space, and the music would be designed to evoke a sense of wonder and awe, while being danceable.', 
+        'A rising synth is playing an arpeggio with a lot of reverb. It is backed by pads, sub bass line and soft drums. This song is full of synth sounds creating a soothing and adventurous atmosphere. It may be playing at a festival during two songs for a buildup.', 
+        'Slow tempo, bass-and-drums-led reggae song. Sustained electric guitar. High-pitched bongos with ringing tones. Vocals are relaxed with a laid-back feel, very expressive.',
+    ],
+    ['song with synths and flute', 'crowd cheering', 'piano sonata waltz, glittery', 'house song, 4 on the floor, rhythm'],
+    ['chirping of birds and the distant echos of bells', 'cat meowing', 'saxophone with drums', 'beethoven piano sonata']
+]
 
-audio_folder = '../audiolm-train/audio'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='run inference on trained musiclm model')
+    parser.add_argument('--semantic_path', required=True, help='path to semantic stage checkpoint')
+    parser.add_argument('--coarse_path', required=True, help='path to coarse stage checkpoint')
+    parser.add_argument('--fine_path', required=True, help='path to fine stage checkpoint')
+    parser.add_argument('--return_coarse_wave', default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument('--clap_path', default='./checkpoints/clap-laion-audioset-fusion.pt')
+    parser.add_argument('--rvq_path', default='./checkpoints/clap.rvq.350.pt')
+    parser.add_argument('--kmeans_path', default='./results/hubert_kmeans/kmeans.joblib')
+    parser.add_argument('--seed', default=0)
 
-print('loading clap...')
-clap_checkpoint = "./checkpoints/clap-laion-audioset-fusion.pt"
-rvq_checkpoint = './results/semantic/semantic.conditioner_rvq.6000.pt'
-with disable_print():
-    clap = create_clap_quantized(device=device, learn_rvq=False, checkpoint_path=clap_checkpoint, rvq_checkpoint_path=rvq_checkpoint).to(device)
+    args = parser.parse_args()
 
-print('loading wav2vec...')
-wav2vec = get_wav2vec(device=device)
+    semantic_path = args.semantic_path
+    coarse_path = args.coarse_path
+    fine_path = args.fine_path
+    return_coarse_wave = args.return_coarse_wave
+    kmeans_path = args.kmeans_path
+    clap_path = args.clap_path
+    rvq_path = args.rvq_path
+    seed = args.seed
 
-print('loading encodec')
-encodec_wrapper = create_encodec_24khz(bandwidth=12.).to(device)
+    print(f'semantic checkpoint {semantic_path}, coarse checkpoint {coarse_path}, fine checkpoint {fine_path}')
+    print(f'kmeans path {kmeans_path}, clap path {clap_path}, rvq path {rvq_path}, return_coarse_wave {return_coarse_wave}')
 
-semantic_transformer = create_semantic_transformer(
-    dim=1024,
-    depth=6,
-    clap_codebook_size=clap.codebook_size,
-    semantic_codebook_size=wav2vec.codebook_size,
-).to(device)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-print('creating transformers')
-coarse_transformer = create_coarse_transformer(
-    dim=1024,
-    depth=6,
-    clap_codebook_size=clap.codebook_size,
-    semantic_codebook_size=wav2vec.codebook_size,
-    acoustic_codebook_size=encodec_wrapper.codebook_size,
-    num_coarse_quantizers=3,
-).to(device)
+    print('loading clap...')
+    with disable_print():
+        clap = create_clap_quantized(device=device, learn_rvq=False, checkpoint_path=clap_path, rvq_checkpoint_path=rvq_path).to(device)
 
-fine_transformer = create_fine_transformer(
-    dim=1024,
-    depth=6,
-    clap_codebook_size=clap.codebook_size,
-    acoustic_codebook_size=encodec_wrapper.codebook_size,
-    num_coarse_quantizers=3,
-    num_fine_quantizers=5,
-).to(device)
+    print('loading wav2vec...')
+    wav2vec = get_hubert_kmeans(
+        model_name='m-a-p/MERT-v0', 
+        kmeans_path=kmeans_path,
+        normalize_input=True,
+        normalize_embeds=True,
+    ).to(device)
 
-def load_model(model, path):
-    path = Path(path)
-    assert path.exists(), f'checkpoint does not exist at {str(path)}'
-    pkg = torch.load(str(path))
-    model.load_state_dict(pkg)
+    print('loading encodec')
+    encodec_wrapper = create_encodec_24khz(bandwidth=6.).to(device)
 
-print('loading semantic stage...')
-load_model(semantic_transformer, './results/semantic/semantic.transformer.6000.pt')
+    semantic_transformer = create_semantic_transformer(
+        dim=1024,
+        depth=6,
+        clap_codebook_size=clap.codebook_size,
+        semantic_codebook_size=wav2vec.codebook_size,
+    ).to(device)
 
-print('loading coarse stage...')
-load_model(coarse_transformer, './results/coarse/coarse.transformer.4000.pt')
+    print('creating transformers')
+    coarse_transformer = create_coarse_transformer(
+        dim=1024,
+        depth=6,
+        clap_codebook_size=clap.codebook_size,
+        semantic_codebook_size=wav2vec.codebook_size,
+        acoustic_codebook_size=encodec_wrapper.codebook_size,
+        num_coarse_quantizers=3,
+    ).to(device)
 
-print('loading fine stage...')
-load_model(fine_transformer, './results/fine/fine.transformer.3000.pt')
+    fine_transformer = create_fine_transformer(
+        dim=1024,
+        depth=6,
+        clap_codebook_size=clap.codebook_size,
+        acoustic_codebook_size=encodec_wrapper.codebook_size,
+        num_coarse_quantizers=3,
+        num_fine_quantizers=5,
+    ).to(device)
 
-print('loading musiclm')
-musiclm = MusicLM(
-    wav2vec=wav2vec,
-    clap=clap,
-    neural_codec=encodec_wrapper,
-    semantic_transformer=semantic_transformer,
-    coarse_transformer=coarse_transformer,
-    fine_transformer=fine_transformer,
-    unique_consecutive=True,
-)
+    def load_model(model, path):
+        path = Path(path)
+        assert path.exists(), f'checkpoint does not exist at {str(path)}'
+        pkg = torch.load(str(path))
+        model.load_state_dict(pkg)
 
-print('generating...')
-generated_wave = musiclm.forward(text=['chirping of birds and the distant echos of bells', 'cat meowing'])
+    print('loading semantic stage...')
+    load_model(semantic_transformer, semantic_path)
 
-print(generated_wave.shape)
+    print('loading coarse stage...')
+    load_model(coarse_transformer, coarse_path)
 
-generated_wave = rearrange(generated_wave, 'b n -> b 1 n')
-for i, wave in enumerate(generated_wave):
-    torchaudio.save(f'results/gen_{i}.wav', wave, encodec_wrapper.sample_rate)
+    print('loading fine stage...')
+    load_model(fine_transformer, fine_path)
+
+    print('loading musiclm')
+    musiclm = MusicLM(
+        wav2vec=wav2vec,
+        clap=clap,
+        neural_codec=encodec_wrapper,
+        semantic_transformer=semantic_transformer,
+        coarse_transformer=coarse_transformer,
+        fine_transformer=fine_transformer,
+    )
+
+    torch.manual_seed(seed)
+
+    print('generating...')
+
+    for prompt in prompts:
+        generated_wave = musiclm.forward(
+            text=prompt, 
+            output_seconds=4, 
+            semantic_window_seconds=8, 
+            coarse_window_seconds=4, 
+            fine_window_seconds=2, 
+            return_coarse_generated_wave=return_coarse_wave,
+        )
+
+        print(generated_wave.shape)
+
+        generated_wave = rearrange(generated_wave, 'b n -> b 1 n')
+        for i, wave in enumerate(generated_wave):
+            torchaudio.save(f'results/{prompt[i][:25]}_generated.wav', wave, encodec_wrapper.sample_rate)
 
--- scripts/infer_coarse.py
+++ scripts/infer_coarse.py
@@ -1,3 +1,19 @@
+'''
+1) load audio samples
+2) compute clap tokens and semantic tokens
+3) run them through coarse stage to predict coarse tokens
+4) reconstruct audio from coarse tokens
+Reconstructed audio should be semantically similar to the original audio if hubert-kmeans and coarse stage are working correctly
+
+example usage:
+
+python scripts/infer_coarse.py \
+    ./data/fma_large/000/000005.mp3 \
+    ./data/fma_large/000/000010.mp3 \
+    --coarse_path ./results/coarse_continue_1/coarse.transformer.10000.pt 
+
+'''
+
 import os
 import sys
 from pathlib import Path
@@ -6,7 +22,7 @@ import torch
 import torchaudio
 from torchaudio.functional import resample
 from einops import rearrange
-
+import argparse
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -16,87 +32,107 @@ from open_musiclm.encodec_wrapper import create_encodec_24khz
 from open_musiclm.hf_hubert_kmeans import get_hubert_kmeans
 from scripts.train_utils import disable_print
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='run inference on coarse stage')
+    parser.add_argument('audio_files', type=str, nargs='+')
+    parser.add_argument('--coarse_path', required=True, help='path to coarse stage checkpoint')
+    parser.add_argument('--clap_path', default='./checkpoints/clap-laion-audioset-fusion.pt')
+    parser.add_argument('--rvq_path', default='./checkpoints/clap.rvq.350.pt')
+    parser.add_argument('--kmeans_path', default='./results/hubert_kmeans/kmeans.joblib')
+    parser.add_argument('--seed', default=0)
 
-# 1) load a sample from the training data
-# 2) compute clap tokens and semantic tokens
-# 3) run them through coarse stage to predict coarse tokens
-# 4) reconstruct audio from coarse tokens
-# Reconstructed audio should be semantically similar to the original audio if hubert-kmeans and coarse stage are working correctly
+    args = parser.parse_args()
 
+    audio_files = args.audio_files
+    coarse_path = args.coarse_path
+    kmeans_path = args.kmeans_path
+    clap_path = args.clap_path
+    rvq_path = args.rvq_path
+    seed = args.seed
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'running inference on {audio_files}')
+    print(f'coarse_path: {coarse_path}, kmeans_path: {kmeans_path}, clap_path: {clap_path}, rvq_path: {rvq_path}, seed: {seed}')
 
-print('loading clap...')
-clap_checkpoint = "./checkpoints/clap-laion-audioset-fusion.pt"
-rvq_checkpoint = './checkpoints/clap.rvq.350.pt'
-with disable_print():
-    clap = create_clap_quantized(device=device, learn_rvq=False, checkpoint_path=clap_checkpoint, rvq_checkpoint_path=rvq_checkpoint).to(device)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-print('loading wav2vec...')
-wav2vec = get_hubert_kmeans(
-    model_name='m-a-p/MERT-v0', 
-    kmeans_path='./results/hubert_kmeans_normalize_embeds/kmeans.joblib',
-    normalize_input=True,
-    normalize_embeds=True,
-).to(device)
+    print('loading clap...')
+    with disable_print():
+        clap = create_clap_quantized(device=device, learn_rvq=False, checkpoint_path=clap_path, rvq_checkpoint_path=rvq_path).to(device)
 
-print('loading encodec')
-encodec_wrapper = create_encodec_24khz(bandwidth=6.).to(device)
+    print('loading wav2vec...')
+    wav2vec = get_hubert_kmeans(
+        model_name='m-a-p/MERT-v0', 
+        kmeans_path=kmeans_path,
+        normalize_input=True,
+        normalize_embeds=True,
+    ).to(device)
 
-print('creating coarse transformer')
-coarse_transformer = create_coarse_transformer(
-    dim=1024,
-    depth=6,
-    clap_codebook_size=clap.codebook_size,
-    semantic_codebook_size=wav2vec.codebook_size,
-    acoustic_codebook_size=encodec_wrapper.codebook_size,
-    num_coarse_quantizers=3,
-).to(device)
+    print('loading encodec')
+    encodec_wrapper = create_encodec_24khz(bandwidth=6.).to(device)
 
-def load_model(model, path):
-    path = Path(path)
-    assert path.exists(), f'checkpoint does not exist at {str(path)}'
-    pkg = torch.load(str(path))
-    model.load_state_dict(pkg)
+    print('creating coarse transformer')
+    coarse_transformer = create_coarse_transformer(
+        dim=1024,
+        depth=6,
+        clap_codebook_size=clap.codebook_size,
+        semantic_codebook_size=wav2vec.codebook_size,
+        acoustic_codebook_size=encodec_wrapper.codebook_size,
+        num_coarse_quantizers=3,
+    ).to(device)
 
-print('loading coarse stage...')
-load_model(coarse_transformer, './results/coarse_continue_1/coarse.transformer.3000.pt')
+    def load_model(model, path):
+        path = Path(path)
+        assert path.exists(), f'checkpoint does not exist at {str(path)}'
+        pkg = torch.load(str(path))
+        model.load_state_dict(pkg)
 
-coarse_stage = CoarseStage(
-    coarse_transformer=coarse_transformer,
-    neural_codec=encodec_wrapper,
-    wav2vec=wav2vec,
-    clap=clap
-)
+    print('loading coarse stage...')
+    load_model(coarse_transformer, coarse_path)
 
-torch.manual_seed(99)
+    coarse_stage = CoarseStage(
+        coarse_transformer=coarse_transformer,
+        neural_codec=encodec_wrapper,
+        wav2vec=wav2vec,
+        clap=clap
+    )
 
-print('loading audio from dataset')
+    torch.manual_seed(99)
 
-data, sample_hz = torchaudio.load('./data/fma_large/000/000005.mp3')
+    print('loading audio from dataset')
+    
+    audios_for_clap = []
+    audios_for_wav2vec = []
+    for audio_path in audio_files:
+        data, sample_hz = torchaudio.load(audio_path)
 
-if data.shape[0] > 1:
-    data = torch.mean(data, dim=0).unsqueeze(0)
+        if data.shape[0] > 1:
+            data = torch.mean(data, dim=0).unsqueeze(0)
 
-target_length = int(4 * sample_hz)
-data = data[:, :target_length]
-audio_for_clap = resample(data, sample_hz, clap.sample_rate)
-audio_for_wav2vec = resample(data, sample_hz, wav2vec.target_sample_hz)
+        target_length = int(4 * sample_hz)
+        data = data[:, :target_length]
+        audio_for_clap = resample(data, sample_hz, clap.sample_rate)
+        audio_for_wav2vec = resample(data, sample_hz, wav2vec.target_sample_hz)
 
-clap_token_ids = get_or_compute_clap_token_ids(None, clap, audio_for_clap.to(device), None)
-semantic_token_ids = get_or_compute_semantic_token_ids(None, audio_for_wav2vec.to(device), wav2vec)
+        audios_for_clap.append(audio_for_clap)
+        audios_for_wav2vec.append(audio_for_wav2vec)
 
-generated_wave = coarse_stage.generate(
-    clap_token_ids=clap_token_ids,
-    semantic_token_ids=semantic_token_ids,
-    coarse_token_ids=None,
-    max_time_steps=int(4 * 75),
-    reconstruct_wave=True,
-    include_eos_in_output=False,
-    append_eos_to_conditioning_tokens=True,
-    temperature=0.95,
-)
+    audios_for_clap = torch.cat(audios_for_clap, dim=0).to(device)
+    audios_for_wav2vec = torch.cat(audios_for_wav2vec, dim=0).to(device)
 
-generated_wave = rearrange(generated_wave, 'b n -> b 1 n').detach().cpu()
-for i, wave in enumerate(generated_wave):
-    torchaudio.save(f'results/{i}.wav', wave, encodec_wrapper.sample_rate)
+    clap_token_ids = get_or_compute_clap_token_ids(None, clap, audios_for_clap, None)
+    semantic_token_ids = get_or_compute_semantic_token_ids(None, audios_for_wav2vec, wav2vec)
+
+    generated_wave = coarse_stage.generate(
+        clap_token_ids=clap_token_ids,
+        semantic_token_ids=semantic_token_ids,
+        coarse_token_ids=None,
+        max_time_steps=int(4 * 75),
+        reconstruct_wave=True,
+        include_eos_in_output=False,
+        append_eos_to_conditioning_tokens=True,
+        temperature=0.95,
+    )
+
+    generated_wave = rearrange(generated_wave, 'b n -> b 1 n').detach().cpu()
+    for i, wave in enumerate(generated_wave):
+        torchaudio.save(f'results/{i}.wav', wave, encodec_wrapper.sample_rate)
