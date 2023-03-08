432d3eb8141822e6fec1aae76bba5e10361a32bc
zhvng <allenjameszhang@gmail.com>
2023-03-08 07:44:51 +0000

    remove librosa depedency

--- open_musiclm/laion_clap/hook.py
+++ open_musiclm/laion_clap/hook.py
@@ -8,7 +8,6 @@ Support: LAION
 import progressbar
 import os
 import torch
-import librosa
 from clap_module import create_model
 from training.data import get_audio_features
 from training.data import int16_to_float32, float32_to_int16
@@ -131,39 +130,39 @@ class CLAP_Module(torch.nn.Module):
         for n in param_names:
             print(n, "\t", "Loaded" if n in ckpt else "Unloaded")
     
-    def get_audio_embedding_from_filelist(self, x):
-        """get audio embeddings from the audio file list
+    # def get_audio_embedding_from_filelist(self, x):
+    #     """get audio embeddings from the audio file list
 
-        Parameters
-        ----------
-        x: List[str] (N,): 
-            an audio file list to extract features, audio files can have different lengths (as we have the feature fusion machanism)
+    #     Parameters
+    #     ----------
+    #     x: List[str] (N,): 
+    #         an audio file list to extract features, audio files can have different lengths (as we have the feature fusion machanism)
         
-        Returns
-        ----------
-        audio_embed : numpy.darray (N,D):
-            audio embeddings that extracted from audio files
-        """ 
-        self.model.eval()
-        audio_input = []
-        for f in x:
-            # load the waveform of the shape (T,), should resample to 48000
-            audio_waveform, _ = librosa.load(f, sr=48000)           
-            # quantize
-            audio_waveform = int16_to_float32(float32_to_int16(audio_waveform))
-            audio_waveform = torch.from_numpy(audio_waveform).float()
-            temp_dict = {}
-            # the 'fusion' truncate mode can be changed to 'rand_trunc' if run in unfusion mode
-            temp_dict = get_audio_features(
-                temp_dict, audio_waveform, 480000, 
-                data_truncating='fusion', 
-                data_filling='repeatpad',
-                audio_cfg=self.model_cfg['audio_cfg']
-            )
-            audio_input.append(temp_dict)
-        audio_embed = self.model.get_audio_embedding(audio_input)
-        audio_embed = audio_embed.detach().cpu().numpy()
-        return audio_embed
+    #     Returns
+    #     ----------
+    #     audio_embed : numpy.darray (N,D):
+    #         audio embeddings that extracted from audio files
+    #     """ 
+    #     self.model.eval()
+    #     audio_input = []
+    #     for f in x:
+    #         # load the waveform of the shape (T,), should resample to 48000
+    #         audio_waveform, _ = librosa.load(f, sr=48000)           
+    #         # quantize
+    #         audio_waveform = int16_to_float32(float32_to_int16(audio_waveform))
+    #         audio_waveform = torch.from_numpy(audio_waveform).float()
+    #         temp_dict = {}
+    #         # the 'fusion' truncate mode can be changed to 'rand_trunc' if run in unfusion mode
+    #         temp_dict = get_audio_features(
+    #             temp_dict, audio_waveform, 480000, 
+    #             data_truncating='fusion', 
+    #             data_filling='repeatpad',
+    #             audio_cfg=self.model_cfg['audio_cfg']
+    #         )
+    #         audio_input.append(temp_dict)
+    #     audio_embed = self.model.get_audio_embedding(audio_input)
+    #     audio_embed = audio_embed.detach().cpu().numpy()
+    #     return audio_embed
 
 
     def get_audio_embedding_from_data(self, x):
