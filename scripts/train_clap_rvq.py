8609a6fb663c8928ab01a15abedc7d919740b486
zhvng <allenjameszhang@gmail.com>
2023-03-14 05:57:43 +0000

    add option to continue from checkpoint when training clap rvq

--- scripts/train_clap_rvq.py
+++ scripts/train_clap_rvq.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     parser.add_argument('--results_folder', default='./results/clap_rvq')
     parser.add_argument('--model_config', default='./configs/model/musiclm_small.json')
     parser.add_argument('--training_config', default='./configs/training/train_musiclm_fma.json')
+    parser.add_argument('--continue_from', default=None, type=str)
 
     args = parser.parse_args()
 
@@ -27,7 +28,7 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     print('loading clap...')
-    clap = create_clap_quantized_from_config(model_config, None, device)
+    clap = create_clap_quantized_from_config(model_config, args.continue_from, device)
 
     trainer = create_clap_rvq_trainer_from_config(
         model_config=model_config,
